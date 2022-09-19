1a28f0ed445e0e40638d530759d10d46307a0558
Haunani Pao <90392354+haunanipao@users.noreply.github.com>
2022-09-19 23:52:36 +1200

    React Version - clean up

--- server/db/db.js
+++ server/db/db.js
@@ -2,8 +2,6 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
-// const connection = require('./connection')
-
 module.exports = {
   getWow,
   addWow,
