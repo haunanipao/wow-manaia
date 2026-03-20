b975ee98a4d9da6b20868c809f8e4ec8648223e2
Haunani Pao <90392354+haunanipao@users.noreply.github.com>
2026-03-20 18:50:41 +1300

    Small Update

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -3,20 +3,21 @@ import React from 'react'
 export default function Card({ info }) {
   return (
     <>
-      {info.map((wow) => {
-        return (
-          <div to={`/`} key={wow.id}>
-            <div className="card gradient-pattern">
-              <h2 className="wowQuote" key={wow.quote}>
-                {wow.quote}
-              </h2>
-              <h2 className="wowName" key={wow.name}>
-                ~ {wow.name}
-              </h2>
+      {info &&
+        info.map((wow) => {
+          return (
+            <div to={`/`} key={wow.id}>
+              <div className="card gradient-pattern">
+                <h2 className="wowQuote" key={wow.quote}>
+                  {wow.quote}
+                </h2>
+                <h2 className="wowName" key={wow.name}>
+                  ~ {wow.name}
+                </h2>
+              </div>
             </div>
-          </div>
-        )
-      })}
+          )
+        })}
     </>
   )
 }
